a7d85579ab862fc82829e844ecf2b39152a6e3d9
Gemmus <112064697+Gemmus@users.noreply.github.com>
2024-02-25 16:20:55 +0200

    Add files via upload

---
+++ Backend/CMakeFiles/DataStructAlg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructAlg.dir/main.cpp.o"
+  "CMakeFiles/DataStructAlg.dir/main.cpp.o.d"
+  "DataStructAlg"
+  "DataStructAlg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructAlg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
